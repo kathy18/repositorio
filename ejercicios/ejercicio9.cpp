1ed8afbd6cba3942f46dac385e3b3e8254972467
kathy18 <katherine.jimenez@ucsp.edu.pe>
2017-04-28 12:44:59 -0500

    Add files via upload

---
+++ ejercicios/ejercicio9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void insertionsort(int a[],int size){
+	int i,key,temp;
+	for(i=1;i<size;i++){
+		key=i;
+		while(key>0 && a[key]< a[key-1]){
+			temp=a[key];
+			a[key]=a[key-1];
+			a[key-1]=temp;
+			key--;
+		}
+	}
+
+	int j;
+	for(j=0;j<size;j++){
+		cout<<a[j]<<" ";
+	}
+}
+
+
+int main(){
+	int a[10]={10,2,5,3,40,8,7,1,6,9};
+	insertionsort(a,10);
+
+}
