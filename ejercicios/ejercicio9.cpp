6e61dd21b20eea79174f8a59f694b42848ab3609
kathy18 <katherine.jimenez@ucsp.edu.pe>
2017-04-28 12:50:33 -0500

    Update ejercicio9.cpp

--- ejercicios/ejercicio9.cpp
+++ ejercicios/ejercicio9.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void insertionsort(int a[],int size){
-	int i,key,temp;
-	for(i=1;i<size;i++){
-		key=i;
-		while(key>0 && a[key]< a[key-1]){
-			temp=a[key];
-			a[key]=a[key-1];
-			a[key-1]=temp;
-			key--;
+void insertionsort(int a[],int t){
+	int i,valor,temp;
+	for(i=1;i<t;i++){
+		valor=i;
+		while(valor>0 && a[valor]< a[valor-1]){
+			temp=a[valor];
+			a[valor]=a[valor-1];
+			a[valor-1]=temp;
+			valor--;
 		}
 	}
 
 	int j;
-	for(j=0;j<size;j++){
+	for(j=0;j<t;j++){
 		cout<<a[j]<<" ";
 	}
 }
 
 
 int main(){
-	int a[10]={10,2,5,3,40,8,7,1,6,9};
+	int a[10]={1,2,4,3,50,8,7,14,23,9};
 	insertionsort(a,10);
 
 }
