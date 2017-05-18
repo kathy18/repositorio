e96d0dc46d03d1ac9c475624f13450469ec37c6b
kathy18 <katherine.jimenez@ucsp.edu.pe>
2017-05-18 16:08:35 -0500

    Add files via upload

---
+++ numeroscomplejos/include/complejos.h
@@ -0,0 +1,26 @@
+#ifndef COMPLEJOS_H
+#define COMPLEJOS_H
+
+//nombrar funciones
+class complejo
+{
+    private:
+        double real, imaginaria;
+    public:
+        complejo(double x, double y){
+        real=x;
+        imaginaria=y;
+        }
+        complejo suma(complejo &a){
+            complejo s;
+            real+a.real;
+            imaginaria=a.imaginaria
+            return s;
+        }
+        complejo_resta();
+        complejo_producto_                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        escalar();
+        complejo_multiplicacion();
+
+};
+
+#endif // COMPLEJOS_H
