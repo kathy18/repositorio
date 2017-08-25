41686f31de1b73c500dc1dfa2f0669d69eb8f563
kathy18 <katherine.jimenez@ucsp.edu.pe>
2017-08-25 15:07:22 -0500

    Add files via upload

---
+++ euclidesextendido.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int modulo (int a, int b){
+	int c,r;
+	c=a/b;
+	r=a-(b*c);
+	return r;
+} 
+int mcd(int a,int b) 
+{ 
+	int c,r; 
+	c = a/b; 
+	r = modulo(a,b); 
+	if(r == 0) {
+		return abs(b);  
+	}
+	else 
+		mcd(b,r); 
+} 
+int euclides_extendido(int a,int b){
+	int u =1;
+	int v=0;
+	int d=(a*u)+(b*v);
+	int e=(a*v)+(b*u);
+	if (b == 0)  {  
+		mcd = a;
+		u = 1;
+		v = 0;
+	}
+	else
+	{     
+		euclides_extendido (b,a%b,mcd,u,v);
+		x1= x2; y1=y2; x2=y1;
+		y2=x1- (a/b)*y1;   
+	}
+	return mcd;
+} 
+}
+int main() 
+{ 
+	int num1,num2; 
+	cout<<"ingrese dos numeros numeros"<<endl; 
+	cin>>num1;
+	cin>>num2; 
+	if(num1>num2) 
+		cout<<"mcd de "<<num1<<" y "<<num2<<" es "<<mcd(num1,num2); 
+	else if(num2>num1) 
+		cout<<"mcd de"<<num1<<" y "<<num2<<" es "<<mcd(num2,num1); 
+	return 0; 
+} 
